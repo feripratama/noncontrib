10b5935b863bcffbdeb9cebc758ddd0a1717b8c4
feripratama <32523275+feripratama@users.noreply.github.com>
2017-11-14 16:31:43 +0700

    Create MyClass.php

---
+++ MyClass.php
@@ -0,0 +1,19 @@
+<?php
+
+  namespace MyClass;
+
+  class MyClass{
+    
+    protected $where;
+  
+    public function where($val)
+    {
+       $this->where = $val;
+    }
+    
+    public function getData()
+    {
+      
+    }
+  
+  }
